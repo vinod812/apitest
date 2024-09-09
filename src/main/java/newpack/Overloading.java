f684b50694ba88fbab8ef020afb0f3ac251f6a46
VinodVerma <42334907+vinod812@users.noreply.github.com>
2024-09-09 15:09:41 +0530

    Create Overloading.java

---
+++ src/main/java/newpack/Overloading.java
@@ -0,0 +1,9 @@
+package newpack;
+import org.testng.annotations.Test;
+
+public class Overloading {
+  @Test
+  public void Overloading() throws Exception {
+    System.out.println("Overloading");
+  }
+}
