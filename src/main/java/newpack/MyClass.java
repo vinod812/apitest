40932adc46f3f5e4453a2500f491420944d409fb
VinodVerma <42334907+vinod812@users.noreply.github.com>
2024-09-09 14:56:56 +0530

    Create MyClass.java

---
+++ src/main/java/newpack/MyClass.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class MyClass {
+  @Test
+  public void restTesting99() throws Exception {
+    int x=10;
+    int y=25;
+    int z=x+y;
+
+    System.out.println("Sum of x+y = " + z);
+  }
+}
