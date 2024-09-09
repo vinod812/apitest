1052c02360d7498875eac4a0702c061c0241b978
VinodVerma <42334907+vinod812@users.noreply.github.com>
2024-09-09 15:08:31 +0530

    Create ClassObject.java

---
+++ src/main/java/newpack/ClassObject.java
@@ -0,0 +1,9 @@
+package newpack;
+import org.testng.annotations.Test;
+
+public class ClassObject {
+  @Test
+  public void classObject() throws Exception {
+    System.out.println("Class Object");
+  }
+}
